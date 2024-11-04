18d1fb8154e77d63779715899cd52e0ba8e4c481
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2024-11-04 13:08:41 +1300

    Basic Chat

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Serve your HTML file
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('signal', (data) => {
+        socket.broadcast.emit('signal', data); // Broadcast signal to other peers
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
